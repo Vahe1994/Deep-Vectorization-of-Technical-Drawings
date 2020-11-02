5dcfb97d4e250b7f97970259e31006e2be9a5077
Vahe1994 <vahe527887@yandex.ru>
2020-11-02 16:05:15 +0300

    Added downladable datasets of abc and backgrounds

---
+++ dataset/download_datasets.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILEID=1hET43eM2cfwfqI7g1VsusU1wNpvc2VHl
+FILENAME=abc.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O $FILENAME && rm -rf /tmp/cookies.txt
+
+FILEID=13eJ9PSYg3QgSuTE4nXgnVVl17k6lTEb7
+FILENAME=background.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O $FILENAME && rm -rf /tmp/cookies.txt
+
