bb3f361dc939623470b11d386ee1201a6ace6405
Vahe1994 <vahe527887@yandex.ru>
2020-11-26 16:22:08 +0300

    A little modification to download_dataset.sh

--- dataset/download_datasets.sh
+++ dataset/download_datasets.sh
@@ -9,6 +9,10 @@ wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download
 FILEID=13eJ9PSYg3QgSuTE4nXgnVVl17k6lTEb7
 FILENAME=background.zip
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O $FILENAME && rm -rf /tmp/cookies.txt
+unzip background.zip
+rm background.zip
+rm -rf __MACOSX
+
 
 FILEID=1dgJLgtPvk9SK9rOCnw-WQRBG6OsMUEH3
 FILENAME=Dataset_of cleaning.zip
